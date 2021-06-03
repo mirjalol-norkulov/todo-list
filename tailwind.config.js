1f46bc18ff59d4fef44b9739ad28aec6b82e6b75
Mirjalol Norqulov <mirjalol2401@gmail.com>
2021-06-03 11:45:52 +0500

    added purge css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./src/**/*.vue"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
